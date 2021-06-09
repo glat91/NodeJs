0150f9bc222a92196e87a65e05662db223c372d5
glat91 <71050732+glat91@users.noreply.github.com>
2021-06-08 23:28:46 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,41 @@
 const express = require('express');
 const { path } = require('path');
 const router = express.Router();
-//const indexController = require()
 /*
-                Rutas Syncronas
+                Synchronous Rute
 */
 // Forma Basica esto se pasara a controllers
 router.get('/', (req, res) => {
     res.render('index', {
-        pasarVariable: "pasar variable" // forma de pasar datos entre ventanas
+        passVar: "Data Sending" // Way to pass values between pages
     })
 });
-// Diferentes datos de los request
+// Diffrents type of data recive
 router.post('/loggin/:userId', (req, res) =>{
-    const userId = req.params || {}; // Chequeo si exsiste
-    const test = req.query || {}; // Obtener cadena creada
-    const {name, age} = req.body || {}; // body de la peticion
+    const userId = req.params || {}; // Verify if params exsist
+    const test = req.query || {}; // Get string created
+    const {name, age} = req.body || {}; // Body request
     
-    const ip = req.ip; // Obtener Ip
-    const contentType = req.headers['content-type'] // regresa los diferentes headers
-    const cookie = req.cookies; // Obtenemos los cookies
+    const ip = req.ip; // Get Ip
+    const contentType = req.headers['content-type'] // Get headers
+    const cookie = req.cookies; // Get Cookies
 
     req.json({
-        id: userId, // por urlEncode
+        id: userId,
         nombre: name,
         edad : age
     })
 })
 // Status
 router.get('/status', (req, res) => {
-    res.status(333).json({ // Envia el status con mensaje
+    res.status(333).json({ // Send Status with message
         error: 'Accseso denegado',
         code: 74456
     })
-    //res.sendStatus(401); // Envia solo el status
+    //res.sendStatus(401); // Send Only Status
 })
-// Enviar Header personalizado
-router.get('/headder-send', (req, res) => {
+// Send own header
+router.get('/header-send', (req, res) => {
     res.set({
         'Content-Type': 'application/json',
         'mi-cache': 'valor propio'
@@ -44,36 +43,34 @@ router.get('/headder-send', (req, res) => {
         status: 'ok'
     })
 })
-// Redireccionar
+// Redirect
 router.get('/redirect', (req, res) => {
     res.redirect('https://www.google.com')
 })
-// Mandar Archivo
+// Send file
 router.get('/send-txt', (req, res) => {
     res.download(__dirname + 'nobre_archivo.txt'); // se agrega el path del archivo
 })
 /*
-            Rutas Asyncronas
+            Asynchronous Routes
 */
-// Primero se tiene q crear la funcion asyncrona
+// Async Function
 async function sleep(ms){
     return new Promise((resolve, reject) => setTimeout(resolve, ms));
-    //return new Promise((resolve, reject) => setTimeout(reject(new Error('Problema en el servidor')), ms)) // Mandar error
 }
-// Segundo se declara el routeHelper Para manejo de errores
+// Route Helper for Error Manage
 async function routeHelper(callback){
-    return async (req, res) => { // Regresamos una funcion asyncrona que recibe los parametros comunes de las rutas en las que se usara
+    return async (req, res) => {
         try{
             await callback(req, res);
         }
         catch(error){
             res.status(400).json({
-                status: error // Verificar error antes de enviarlo
+                status: error
             });
         }
     }   
 }
-// Se usa en la ruta
 router.get('/', routeHelper(async(req, res) => {
     await sleep(500);
     res.json({
@@ -81,5 +78,4 @@ router.get('/', routeHelper(async(req, res) => {
     });
 }));
 
-// Export para uso
 module.exports = router;
